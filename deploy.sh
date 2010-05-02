281bbbe75607bce256b5451f3167f09572268345
Chris Christensen <christian.christensen@gmail.com>
2010-05-02 02:11:32 -0500

    Inital commit

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+../../../appengine_java/appengine-java-sdk-1.2.5/bin/appcfg.sh update www
---
+++ getresin.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -c http://www.caucho.com/download/resin-4.0.6.zip
+unzip resin-4.0.6.zip
+mkdir ./src/WEB-INF/lib
+cp ./resin-4.0.6/lib/resin.jar ./src/WEB-INF/lib/resin.jar
+
